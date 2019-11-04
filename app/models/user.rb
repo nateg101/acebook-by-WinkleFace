52730ed443f984881f42790820bcc9213f38741a
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-11-04 17:18:52 +0000

    Added bcrpyt gem and started set up of password encryption.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+  attr_accessor :password
+  
+  has_secure_password
+
+  include BCrypt
+
+  def self.encrypt(password)
+    BCrypt::Password.create(password)
+  end
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6, maxmium: 10 }, on: :create
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,20 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe User do
+  it 'creates a new user' do
+    user = User.create(
+      :forename => 'Joe',
+      :surname => 'Bloggs',
+      :email => 'noddy@hotmail.com',
+      :username => 'JoeBloggs',
+      :password_digest => 'password123',
+      :password_confirmation => 'password123'
+    )
+    expect(user).to be_a User
+    expect(user.username).to eq 'JoeBloggs'
+    expect(user.email).to eq 'noddy@hotmail.com'
+    expect(user.forename).to eq 'Joe'
+  end
+end
