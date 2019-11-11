f5040ce25564632636382cb357666ed6786ca305
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-11-11 15:55:40 +0000

    Added tests for liking posts in like controller

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -14,15 +14,35 @@ RSpec.describe LikesController, type: :controller do
 
       expect(@user.voted_on?(post)).to be true
       expect(@user.voted_for?(post)).to be true
+      expect(@user.find_up_voted_items.size).to eq(1)
     end
 
     it 'unlikes a post' do
       post = Post.create(message: "Hello, world!", user_id: @user.id)
       like = post.vote_by voter: @user
-       # post.votes_for.down.by_type(@user)
-      post.unliked_by @user
+      unlike = post.unliked_by @user
 
-      # expect(post.vote_flag).to be false
+      expect(@user.find_up_voted_items.size).to eq(0)
+    end
+  end
+
+  describe 'Post /posts/:post_id/comment/:comment_id/' do
+    it 'likes a comment' do
+      post = Post.create(message: "Hello, world!", user_id: @user.id)
+      comment = Comment.create(message: "Comment", user_id: @user.id, post_id: post.id)
+      like = comment.vote_by voter: @user
+
+      expect(@user.voted_on?(comment)).to be true
+      expect(@user.voted_for?(comment)).to be true
+      expect(@user.find_up_voted_items.size).to eq(1)
+    end
+
+    it 'unlikes a comment' do
+      post = Post.create(message: "Hello, world!", user_id: @user.id)
+      comment = Comment.create(message: "Comment", user_id: @user.id, post_id: post.id)
+      like = comment.vote_by voter: @user
+      unlike = comment.unliked_by @user
+
       expect(@user.find_up_voted_items.size).to eq(0)
     end
   end
