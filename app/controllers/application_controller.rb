f0c684daf47f6cee5351d87c669a7add2b84244f
kevindhanna <kevindhanna@gmail.com>
2019-11-14 12:10:50 +0000

    moved cors server allow to ENV file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
   end
 
   def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    headers['Access-Control-Allow-Origin'] = ENV['REACT_SERVER_URL']
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = '*'
     headers['Access-Control-Max-Age'] = "1728000"
